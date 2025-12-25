2f3dc1370a4e823724f53c96af7b41a55c610662
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <opvampire57@gmail.com>
2025-12-25 19:32:28 +0530

    Update Sona_BAN.py

--- VampireMusic/utils/Sona_BAN.py
+++ VampireMusic/utils/Sona_BAN.py
@@ -1,4 +1,6 @@
 from pyrogram import filters
 
 def admin_filter():
-    return filters.admin
+    async def func(_, __, message):
+        return bool(message.from_user and message.from_user.is_admin)
+    return filters.create(func)

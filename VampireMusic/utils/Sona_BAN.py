15360604be023e8977612918b6e76ad6df403564
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <opvampire57@gmail.com>
2025-12-25 19:42:44 +0530

    Update admin filter to use custom function
    Refactor admin filter to check user status in chat.

--- VampireMusic/utils/Sona_BAN.py
+++ VampireMusic/utils/Sona_BAN.py
@@ -1,3 +1,12 @@
 from pyrogram import filters
 
-admin_filter = filters.admin
+def admin_filter(_, __, message):
+    if not message.from_user:
+        return False
+    try:
+        member = message.chat.get_member(message.from_user.id)
+        return member.status in ("administrator", "creator")
+    except:
+        return False
+
+admin_filter = filters.create(admin_filter)

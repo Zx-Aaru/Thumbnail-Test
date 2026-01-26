a41dfeabeffa4c9686c593d15b3f3689530ff92e
ISTKHAR-CODER786 <istkharalam6202@gmail.com>
2026-01-26 11:32:08 +0530

    Update call.py

--- VampireMusic/core/call.py
+++ VampireMusic/core/call.py
@@ -1,10 +1,8 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+def capture_internal_err(error):
+    try:
+        print(f"[INTERNAL ERROR] {error}")
+    except Exception:
+        pass
 import asyncio
 import os
 from datetime import datetime, timedelta

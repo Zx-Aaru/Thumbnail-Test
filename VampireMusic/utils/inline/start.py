524dd91443595c3ba75f66f2572f76f29080f686
Zx-Aaru <armaanmansuri786@yahoo.com>
2026-02-25 16:00:09 +0530

    Update start.py

--- VampireMusic/utils/inline/start.py
+++ VampireMusic/utils/inline/start.py
@@ -18,35 +18,36 @@ import config
 from VampireMusic import app
 
 
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"], callback_data="help_pannel"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_5"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+        ],
+    ]
+    return buttons
+
+# Pichli baar wala start_panel (Ise bhi check kar lein)
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_4"],
-                callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_6"],
-                url=config.SUPPORT_CHANNEL
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_2"],
-                url=config.SUPPORT_CHAT
             ),
         ],
         [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                user_id=config.OWNER_ID
-            )
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_3"], callback_data="help_pannel"),
         ],
     ]
     return buttons
+    
